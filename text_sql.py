c13589bdaf47feb75d0ae09defa261a13880440c
gl-coding <1451607278@qq.com>
2020-11-30 00:23:27 +0800

    output

--- text_sql.py
+++ text_sql.py
@@ -1,5 +1,7 @@
+#encoding=utf8
 import os
 import sys
+import xlwt
 
 class Sql:
 
@@ -9,6 +11,7 @@ class Sql:
         self.global_list = []
         self.filename = filename
         self.final_res = []
+        self.print_type = "print"
         self.load_data()
 
     def format_check(self):
@@ -26,6 +29,9 @@ class Sql:
 
     def get_val_idx(self):
         return self.val_idx
+    
+    def set_print_type(self, print_type="file"):
+        self.print_type = print_type
 
     def load_data(self):
         with open(self.filename) as f:
@@ -35,7 +41,6 @@ class Sql:
 
     def groupby(self, seg_list):
         groupby_dic = {}
-        print "aaaaaaaaaaaa"
         val_idx  = self.get_val_idx()
         for items in self.global_list:
             key = []
@@ -53,6 +58,7 @@ class Sql:
         #print res
         return res
 
+    #by col in cur cols
     def sortby(self, seg_str):
         sortby_dic = []
         seg_list = seg_str.split("\t")
@@ -71,10 +77,13 @@ class Sql:
         res = sortby_dic.sort()
         res = [(k+"\t"+v).split("\t") for k, v in res]
         return res
+    
+    def simple_sort(self):
+        self.final_res.sort()
 
     #eg. select 1 groupby 2 3 sortby 2
     def parse_sql(self, sql):
-        print sql
+        #print sql
         sql = sql + " "
         res = []
         keywords = ["select", "groupby", "sortby"]
@@ -85,27 +94,28 @@ class Sql:
                 continue
             res.append(idx)
         res.append(-1)
-        print res
+        #print res
         interval = [[res[i], res[i+1]] for i in range(len(res)-1)]
-        print keywords
-        print interval
+        #print keywords
+        #print interval
         res_dic = zip(keywords, interval)
-        print res_dic
+        #print res_dic
         arg_dic = []
         for k, v in res_dic:
             key = k
             val = sql[v[0]:v[1]]
             val = val.replace(key, "").strip()
-            val = [int(i) for i in val.split(" ") if i != ""]
+            #print val
+            val = [int(i.strip()) for i in val.split(",") if i != ""]
             arg_dic.append((key, val))
-        print arg_dic
+        #print arg_dic
         return arg_dic
 
     def run_sql(self, sql_str):
         res = []
         arg_dic = self.parse_sql(sql_str)
         for k, v in arg_dic:
-            print k
+            #print k
             if k == "select":
                 if len(v) > 1:
                     return -1
@@ -115,16 +125,56 @@ class Sql:
             if k == "sortby":
                 res = self.sortby(v)
         self.final_res = res
+        self.simple_sort()
+        self.set_print_type()
         self.format_data()
     
     def format_data(self):
-        for items in self.final_res:
-            print "\t".join(items)
-        
+        if self.print_type == "print":
+            for items in self.final_res:
+                print("\t".join(items))
+        elif self.print_type == "file":
+            out_dir  = "output/"
+            out_file = out_dir + "format.res"
+            if not os.path.exists(out_dir):
+                os.mkdir(out_dir)
+            with open(out_file, "a") as f:
+                for items in self.final_res:
+                    line = "\t".join(items) + "\n"
+                    f.write(line)
+        else:
+            in_dir   = "output/"
+            wb = xlwt.Workbook()
+            file_list = []
+            for root, dirs, files in os.walk(in_dir):
+                for f in files:
+                    file_list.append(f)
+            file_list = sorted(file_list)
+
+            for f in file_list:
+                ws = wb.add_sheet(f)
+                full_path = in_dir + f
+                with open(full_path) as fin:
+                    line_count = -1
+                    for line in fin:
+                        line_count += 1
+                        res = line.split("\t")
+                        count = -1
+                        for item in res:
+                            count += 1
+                            #if line_count > 0 and count > 1:
+                            #    item = float(item)
+                            ws.write(line_count, count, item)
+
+            wb.save('res.xls')
+                    
 if __name__ == "__main__":
     filename = sys.argv[1]
     sql = Sql(filename)
     #sql_str = "select 0 groupby 1 sortby 1"
-    sql_str = "select 0 groupby 1"
+    #sql_str = "select 0 groupby 1, 2"
+    sql_str = "select 0 groupby 3, 1"
+    sql_str = "select 0 groupby 3, 2"
     sql.run_sql(sql_str)
-    
+    sql.set_print_type("excel")
+    sql.format_data()
